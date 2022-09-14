6efbc0f05672c0ae8ea11e59f85ac03f5e9fdaf6
Eduardo Denis <edenise@frd.utn.edu.ar>
2022-09-14 20:24:48 -0300

    add card + like

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ function showModal(event) {
 }
 
 var modal = document.getElementById('myModal');
-var cardImgList = document.getElementsByClassName("card-img");
 
+var cardImgList = document.getElementsByClassName("card-img");
 for (let index = 0; index < cardImgList.length; index++) {
     const cardImg = cardImgList[index];
     cardImg.addEventListener("click", showModal);
@@ -28,4 +28,55 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// Agregar like
+
+function like(event) {
+    event.target.classList.toggle('like');
+}
+
+var icono = document.querySelector('.fa-heart');
+icono.addEventListener("click", like);
+// icono.onclick =  like;
+
+
+// Agregar Targeta
+function agregarCard() {
+    // cotenedor de cards
+    var cardContainer = document.getElementById('card-container');
+    
+    var card = document.createElement('div');
+    card.classList.add('card')
+
+    var imagen = document.createElement('img');
+    imagen.classList.add('card-img');
+    imagen.setAttribute('src', './img/cat.png');
+    imagen.setAttribute('alt', 'palceholder');
+    imagen.addEventListener("click", showModal);
+    card.appendChild(imagen);
+
+
+    var icono = document.createElement('i');
+    icono.classList.add('fa-solid', 'fa-heart');
+    icono.onclick =  like;
+
+    var actions = document.createElement('div');
+    actions.classList.add('card-actions');
+
+    actions.appendChild(icono);
+    card.appendChild(actions);
+
+    cardContainer.appendChild(card);
+}
+
+// Agregar favorritos
+function verFavoritos() {
+    console.log("verFavoritos");
+
+    // 1. Conseguir con el document.querySelectorAll todos los iconos que tienen la clase fa-heart
+    // 2. Iterar el resultado, y verificar si el icono tiene la clase 'like'
+    // 3. Si no tiene la clase like, tengo que ocultar al padre del padre
+
+
 }
